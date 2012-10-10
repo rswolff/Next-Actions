46e46192b62989aecc8dd3a4fe0c871a159e1d94
Scott Wolff <scott.wolff@tuscan.ca>
2012-10-10 12:57:44 -0600

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :crypted_password, :email, :first_name, :last_name, :salt
+  attr_accessible :crypted_password, :email, :first_name, :last_name, :salt  
+
+  attr_accessible :email, :password, :password_confirmation
+  authenticates_with_sorcery!
+  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Next::Application.routes.draw do
+  get "pages/home"
+
+  get "user_sessions/new"
+
+  get "user_sessions/create"
+
+  get "user_sessions/destroy"
+
   root :to => 'pages#home'
 
   resources :users
